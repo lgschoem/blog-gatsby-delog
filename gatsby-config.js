b18e8b9e65d36f70d694f3340c74784296f105e0
lynton-dev <61889203+lynton-dev@users.noreply.github.com>
2020-10-07 07:40:19 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/_data/assets`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [{
@@ -53,7 +60,7 @@ module.exports = {
         icon: "src/images/icon.png",
       },
     },
-    `gatsby-plugin-sass`, 
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
     'gatsby-plugin-dark-mode',
